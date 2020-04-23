21741af59957bbec068bc64b1f4964b0abfb7904
angeloxenakis <angeloxenakis@icloud.com>
2020-04-23 16:27:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+const submitData = function(name, email) {
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+        document.body.innerHTML = object.id
+    })
+    .catch(function(error) {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message;
+    });
+}
+
+submitData("Steve", "steve@steve.com")
