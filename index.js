06c4e1ae641125f6ba4cd144baba74a7bede7ebd
Rooted1 <ruthobey@gmail.com>
2020-04-23 21:15:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Add your code here
+// // Add your code here
+// let formData  = {
+//   dogName: "Bryon",
+//   dogBreed: "Poodle"
+// }
+//
+// let configObj = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   }
+//
+//
+// fetch("http://localhost:3000/dogs", configObj)
+//   .then((response)=>{
+//     return response.json()
+//   })
+//   .then((object)=>{
+//     console.log(object)
+//   })
+//   .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   }
+
+let submitData = (name, email) => {
+  return fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        email
+      })
+    })
+    .then(  (response) =>{
+      return response.json()
+    })
+    .then(  (object) =>{
+      document.body.innerHTML = object["id"]
+    })
+    .catch(  (error) =>{
+      document.body.innerHTML = error.message
+    })
+}
